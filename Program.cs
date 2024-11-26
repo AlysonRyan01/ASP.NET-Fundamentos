fa70e9d100c2b6041ce6d9a5022da97d1d45c130
AlysonRyan01 <sdhuahdsaih@gmail.com>
2024-11-26 15:15:11 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,13 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => 
+{
+    return Results.Ok("Hello World");
+});
+app.MapGet("/{nome}", (string nome) => 
+{
+    return Results.Ok($"Hello World {nome}");
+});
+
+app.Run();
